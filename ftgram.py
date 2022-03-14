6f26be341f34aa777938ea6b031253982c2477fd
hurumi <hurumi@gmail.com>
2022-03-14 18:08:17 +0900

    Implement chatbot

--- ftgram.py
+++ ftgram.py
@@ -13,8 +13,10 @@ urllib3.disable_warnings( urllib3.exceptions.InsecureRequestWarning )
 import talib as ta
 import argparse
 import time
-import telegram
 import pandas as pd
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
 from yahooquery import Ticker
 from numpy import NaN
@@ -53,6 +55,9 @@ filter_dict = {
     'RSIUp'        : [ 'RSI',                        params['RSI_H'],  1,   1 ],
     'RSIDn'        : [ 'RSI',                        params['RSI_L'], -1,   1 ],
 }
+
+prev_desc = []
+
 # -------------------------------------------------------------------------------------------------
 # Functions
 # -------------------------------------------------------------------------------------------------
@@ -129,7 +134,7 @@ def get_metric( _info ):
 
     return df
 
-def get_description( _metric ):
+def get_detection( _metric ):
 
     desc = []
 
@@ -157,35 +162,53 @@ def get_description( _metric ):
 
     return desc
 
-def desc_diff( _prev, _new ):
+def check_diff( _prev, _new ):
 
     _temp = [ elem[:elem.index('(')] for elem in _new ]
     if _prev == _temp: return False
     return True
 
+def get_price( _metric ):
+
+    desc = []
+
+    # for each ticker
+    for option in _metric.columns:
+        price = _metric[option]['regularMarketPrice']
+        delta = _metric[option]['regularMarketChangePercent']*100
+        desc.append( f'[{option:4}] {price:.1f} ({delta:.1f}%)')
+
+    return desc
+
+def get_rsi( _metric ):
+
+    desc = []
+
+    # for each ticker
+    for option in _metric.columns:
+        rsi = _metric[option]['RSI']
+        desc.append( f'[{option:4}] {rsi:.1f}')
+
+    return desc
+
 # -------------------------------------------------------------------------------------------------
-# Commandline arguments
+# Callbacks
 # -------------------------------------------------------------------------------------------------
 
-parser = argparse.ArgumentParser( description='Financial Stream', 
-                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter )
-parser.add_argument( '--int', '-i', type=int, default=-1, help='loop interval (seconds) (-1 = no loop)' )
-parser.add_argument( '--print', action='store_true', help='print output to screen' )
-parser.add_argument( '--telegram', action='store_true', help='send output to telegram' )
-args = parser.parse_args()
+def help(update: Update, context: CallbackContext) -> None:
+    """Sends explanation on how to use the bot."""
+    text  = '/help to show usage\n'
+    text += '/start <seconds> to start detector\n'
+    text += '/stop to stop detector\n'
+    text += '/price to show latest price\n'
+    text += '/rsi to show latest rsi\n'
+    update.message.reply_text( text )
 
-# -------------------------------------------------------------------------------------------------
-# Main
-# -------------------------------------------------------------------------------------------------
+def detector(context: CallbackContext) -> None:
+    """Run detector."""
+    global prev_desc
 
-# telegram bot
-bot = telegram.Bot( token = _TELE_TOKEN )
-
-# previous description
-prev_desc = []
-
-# infinite loop
-while True:
+    job = context.job
 
     # get source
     info = get_source( params['port'] )
@@ -193,27 +216,99 @@ while True:
     # get metric
     metric = get_metric( info )
 
-    # get description
-    desc = get_description( metric )
+    # get detection
+    desc = get_detection( metric )
+
+    print( prev_desc, desc )
 
     # output when only entry changes
-    if desc_diff( prev_desc, desc ):
-        
-        # display
-        if args.print:
-            for elem in desc:
-                print( elem )
+    if check_diff( prev_desc, desc ):
+        text = '<code>'+'\n'.join( desc )+'</code>'
+        context.bot.send_message( job.context, text, parse_mode = "HTML" )
 
-        # telegram
-        if args.telegram:
-            text = '<code>'+'\n'.join( desc )+'</code>'
-            bot.sendMessage( chat_id = _TELE_CHAT_ID, text = text, parse_mode = "HTML" )
-    
-    # update previous description
+    # update detection
     prev_desc = [ elem[:elem.index('(')] for elem in desc ]
 
-    # break condition
-    if args.int == -1: break
+def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
 
-    # otherwise
-    time.sleep( args.int )
+def start(update: Update, context: CallbackContext) -> None:
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = int(context.args[0])
+        if due < 0:
+            update.message.reply_text('Sorry we can not go back to future!')
+            return
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_repeating( detector, due, context=chat_id, name=str(chat_id) )
+
+        text = 'Timer successfully set!'
+        if job_removed:
+            text += ' Old one was removed.'
+        update.message.reply_text(text)
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /set <seconds>')
+
+def stop(update: Update, context: CallbackContext) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Timer successfully cancelled!' if job_removed else 'You have no active timer.'
+    update.message.reply_text(text)
+
+def price(update: Update, context: CallbackContext) -> None:
+    """Show latest price"""
+    info   = get_source( params['port'] )
+    metric = get_metric( info )    
+    desc   = get_price ( metric )
+    text = '<code>'+'\n'.join( desc )+'</code>'
+    update.message.reply_text( text, parse_mode = "HTML" )
+
+def rsi(update: Update, context: CallbackContext) -> None:
+    """Show latest RSI"""
+    info   = get_source( params['port'] )
+    metric = get_metric( info )    
+    desc   = get_rsi   ( metric )
+    text = '<code>'+'\n'.join( desc )+'</code>'
+    update.message.reply_text( text, parse_mode = "HTML" )    
+
+# -------------------------------------------------------------------------------------------------
+# Main
+# -------------------------------------------------------------------------------------------------
+
+def main():
+
+    """Run bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater( _TELE_TOKEN )
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler( CommandHandler("help",  help  ) )
+    dispatcher.add_handler( CommandHandler("start", start ) )
+    dispatcher.add_handler( CommandHandler("stop",  stop  ) )
+    dispatcher.add_handler( CommandHandler("price", price ) )
+    dispatcher.add_handler( CommandHandler("rsi",   rsi   ) )
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
+    # SIGABRT. This should be used most of the time, since start_polling() is
+    # non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
